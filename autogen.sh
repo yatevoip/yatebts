669a649717023eb86821ff360d817b9890000c6a
paulc <paulc@26ef948f-17ed-0410-91a8-c94bd273f071>
2013-12-10 15:14:05 +0000

    Added skeleton for configure and packaging files.
    git-svn-id: http://voip.null.ro/svn/yatebts@2 26ef948f-17ed-0410-91a8-c94bd273f071

---
+++ autogen.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Run this to generate a new configure script
+
+if [ -z `which which 2>/dev/null` ]; then
+    echo "Please install the required 'which' utility." >&2
+    exit 1
+fi
+
+ac=`which autoconf 2>/dev/null`
+test -z "$ac" && ac=/usr/local/gnu-autotools/bin/autoconf
+if [ -x "$ac" ]; then
+    "$ac" || exit $?
+    case "x$1" in
+	x--silent)
+	    ;;
+	x--configure)
+	    shift
+	    ./configure "$@" || exit $?
+	    echo "Finished! Now run make."
+	    ;;
+	*)
+	    echo "Finished! Now run configure. If in doubt run ./configure --help"
+	    ;;
+    esac
+else
+    echo "Please install Gnu autoconf to build from CVS." >&2
+    exit 1
+fi
