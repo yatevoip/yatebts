71ae49c8e45d5eecd8fe717cc726e57fc3e6fc19
monica <monica@26ef948f-17ed-0410-91a8-c94bd273f071>
2014-03-17 14:14:53 +0000

    Small fixes: updated command to set write rights and removed call of clearstatcache.
    git-svn-id: http://voip.null.ro/svn/yatebts@179 26ef948f-17ed-0410-91a8-c94bd273f071

--- nib/web/lib/lib_proj.php
+++ nib/web/lib/lib_proj.php
@@ -318,10 +318,8 @@ function test_default_config()
 
 function check_permission($dir)
 {
-	clearstatcache(null, $dir);
-
 	if (!is_dir($dir))
-		return array(false, "The directory: ".$dir ." was not found on this server. Please create the directory and run this as root: 'chmod a+rwx ".$dir."'");
+		return array(false, "The directory: ".$dir ." was not found on this server. Please create the directory and run this as root: 'chmod a+rw ".$dir."'");
 	if (substr(decoct(fileperms($dir)), -4) !== "0777")
 		return array(false, "Don't have permission on ". $dir.". Please run this command as root: 'chmod -R a+rw ".$dir."'");
 
