0ce0eb5e62b04da3bafcbda75a02cc8ed140cf17
paulc <paulc@26ef948f-17ed-0410-91a8-c94bd273f071>
2014-04-24 17:18:12 +0000

    Added copyright and instructions to the roaming script.
    git-svn-id: http://voip.null.ro/svn/yatebts@256 26ef948f-17ed-0410-91a8-c94bd273f071

--- roaming/roaming.js
+++ roaming/roaming.js
@@ -1,3 +1,29 @@
+/**
+ * roaming.js
+ * This file is part of the Yate-BTS Project http://www.yatebts.com
+ *
+ * Copyright (C) 2014 Null Team
+ *
+ * This software is distributed under multiple licenses;
+ * see the COPYING file in the main directory for licensing
+ * information for this specific distribution.
+ *
+ * This use of this software may be subject to additional restrictions.
+ * See the LEGAL file in the main directory for details.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+ * Roaming over SIP interface to OpenVoLTE
+ * To use it put in javascript.conf:
+ *
+ * [scripts]
+ * roaming=roaming.js
+ */
+
 #require "roamingconf.js"
 
 /**
